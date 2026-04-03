6a162db247906e81560d86d5d40608d8bbfade70
[devgalop] <devgalop@gmail.com>
2026-04-03 07:43:46 -0500

    feat: Agrega validadores de campos
    Se agregan los validadores de los campos de entrada

--- src/features/user_management/register_user/register_user_request.py
+++ src/features/user_management/register_user/register_user_request.py
@@ -1,6 +1,40 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+import re
+
+EMAIL_PATTERN = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+USERNAME_PATTERN = r'\w+$'
 
 class RegisterUserRequest(BaseModel):
     username: str
     email: str
     password: str
+    
+    @field_validator('username')
+    def validate_username(cls, value : str) -> str:
+        if len(value) < 3:
+            raise ValueError("Username must be at least 3 characters long")
+        if len(value) > 20:
+            raise ValueError("Username must be no more than 20 characters long")
+        if not re.match(USERNAME_PATTERN, value):
+            raise ValueError("Username can only contain letters, numbers, and underscores")
+        return value
+    
+    @field_validator('email')
+    def validate_email(cls, value : str) -> str:
+        if not re.match(EMAIL_PATTERN, value):
+            raise ValueError("Invalid email address")
+        return value
+    
+    @field_validator('password')
+    def validate_password(cls, value : str) -> str:
+        if len(value) < 6:
+            raise ValueError("Password must be at least 6 characters long")
+        if len(value) > 20:
+            raise ValueError("Password must be no more than 20 characters long")
+        if not any(char.isdigit() for char in value):
+            raise ValueError("Password must contain at least one digit")
+        if not any(char.isalpha() for char in value):
+            raise ValueError("Password must contain at least one letter")
+        if not any(char in "!@#$%^&*()_+-=[]{}|;':,.<>?/" for char in value):
+            raise ValueError("Password must contain at least one special character")
+        return value
